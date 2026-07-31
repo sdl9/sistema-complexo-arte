a648e95370971f298fbeb81dc7681f26dcb6e90d
sdl9 <laidsal99@gmail.com>
2026-07-31 16:07:08 -0300

    refatorado sistema de input

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from venda import Venda
 from itemVenda import ItemVenda
 from catalogo import Catalogo
 
+def ler_inteiro(mensagem):
+    while True:
+        valor_digitado = input(mensagem)
+
+        try:
+            return int(valor_digitado)
+        except ValueError:
+            print ("Digite um número válido.")
+
 # instanciando um produto e uma venda
 
 produto = Produto(1,"Ipa", 20, 10)
@@ -20,27 +29,18 @@ catalogo.listar_produtos()
 continuar = "s"
 
 while continuar == "s":
-    id_digitado = input("Digite o ID do produto: ")
-    try:
-        id_escolhido = int(id_digitado)
-    except ValueError:
-        print("ID inválido. Digite um número de ID.")
-        continue
+
+    id_escolhido = ler_inteiro("Digite o ID do produto: ")
+
     produto_escolhido = catalogo.buscar_produto_por_id(id_escolhido)
 
     if produto_escolhido is None:   
         print("Produto não encontrado.")
             
-    else:
-        quantidade_digitada = input("Quantidade: ")
-        try:
-            quantidade_escolhida = int(quantidade_digitada)
-        except ValueError:
-            print("Quantidade inválida. Digite um número.")
-            continue
+    quantidade_escolhida = ler_inteiro("Digite a quantidade:")
 
-        item = ItemVenda(produto_escolhido, quantidade_escolhida)
-        venda.add_item(item)
+    item = ItemVenda(produto_escolhido, quantidade_escolhida)
+    venda.add_item(item)
 
     continuar = input("Adicionar outro item? (s/n): ").lower()
 
